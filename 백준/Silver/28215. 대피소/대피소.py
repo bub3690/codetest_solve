c2cd0b4723e219e1cfcc204ad85b80f8931e0dbd
Lee Jong Bub <30468434+bub3690@users.noreply.github.com>
2025-03-03 17:03:19 +0900

    [Silver IV] Title: 대피소, Time: 980 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -2,7 +2,7 @@ import sys
 sys.setrecursionlimit(1000000)
 
 
-N,K = tuple(map(int,input().split()))
+N, K = tuple(map(int,input().split()))
 homes = [ list( map(int,input().split()) ) for _ in range(N) ]
 # N개 집. (x,y).
 
@@ -16,8 +16,9 @@ def combination(here, depth):
     global min_answer
 
     if depth==K:
+
         # 모든 집과 거리를 계산.
-        max_house_temp = -1
+        max_house_temp = 0
         for i in range(N):
             # i는 모든집. 
             # i가 K개안에 선정되었다면 넘어가야함.
@@ -28,13 +29,14 @@ def combination(here, depth):
             for j in range(K):
                 dist_x = abs(homes[i][0] - homes[temp_combination[j]][0])
                 dist_y = abs(homes[i][1] - homes[temp_combination[j]][1])
-                dist_xy = dist_x+dist_y
+                dist_xy = dist_x + dist_y
                 min_answer_temp = min(min_answer_temp, dist_xy) # 가장 가까운 대피소 거리.
             
             # 그리고 모든 집들중에 최대 거리 찾고.
             max_house_temp = max(max_house_temp, min_answer_temp)
+            
         # 최대거리중 최소값 업데이트
-        min_answer = min(min_answer,max_house_temp)
+        min_answer = min(min_answer, max_house_temp)
         return
 
     #home을 뽑고 그다음. 집을 선택하는 dfs
