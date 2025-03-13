72aa50afd84c211806fba467062417152803408b
Lee Jong Bub <30468434+bub3690@users.noreply.github.com>
2025-03-13 12:50:12 +0900

    [Gold IV] Title: 알파벳, Time: 3184 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,55 +1,37 @@
 import sys
-sys.setrecursionlimit(100000)
 
+# 입력 받기
+R, C = map(int, input().split())
+grid = [list(input().strip()) for _ in range(R)]
 
-R,C = tuple(map(int,input().split()))
+# 방향 벡터 (우, 하, 좌, 상)
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
 
-grid = [ list(input().strip()) for _ in range(R)]
+# 범위 체크 함수
+def range_check(x, y):
+    return 0 <= x < R and 0 <= y < C
 
-visit = [0] *26
+# 스택을 이용한 DFS
+def stack_based_dfs():
+    max_count = 1  # 최대 탐색 깊이
+    stack = [(0, 0, 1, 1 << (ord(grid[0][0]) - ord('A')))]  # (x, y, count, visited_mask)
 
-# print( ord('Z')- ord('A') )
-dxs= [0,1,0,-1]
-dys= [1,0,-1,0]
+    while stack:
+        x, y, count, visited_mask = stack.pop()
+        max_count = max(max_count, count)
 
-def range_check(x,y):
-    return 0<=x<R and 0<=y<C
+        if max_count == 26:  # 모든 알파벳을 방문했을 경우
+            return 26
 
+        for i in range(4):
+            nx, ny = x + dxs[i], y + dys[i]
+            if range_check(nx, ny):
+                char_idx = ord(grid[nx][ny]) - ord('A')
+                if not (visited_mask & (1 << char_idx)):  # 방문한 적 없는 알파벳이면
+                    stack.append((nx, ny, count + 1, visited_mask | (1 << char_idx)))
 
-answer = -1
-find_answer= False
-def go(here,count):
-    global answer
-    global find_answer
-    now_x, now_y = here
+    return max_count
 
-    answer = max(answer,count)
-
-    if answer>=26:
-        find_answer = True
-        return
-    
-    for i in range(4):
-        next_x = now_x+dxs[i]
-        next_y = now_y+dys[i]
-
-        if range_check(next_x,next_y):
-            if visit[ ord(grid[next_x][next_y])-ord('A') ] ==0 :
-                visit[ ord(grid[next_x][next_y])-ord('A') ] = 1
-                go( (next_x,next_y), count+1)
-                if find_answer:
-                    break
-
-                visit[ ord(grid[next_x][next_y])-ord('A') ] = 0
-
-
-
-visit[ord(grid[0][0])-ord('A') ] = 1
-go((0,0),1)
-
-print(answer)
-
-
-
-
-
+# 결과 출력
+print(stack_based_dfs())
