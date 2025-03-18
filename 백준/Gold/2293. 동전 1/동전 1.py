6a3bde50c7c8d48476dcfec39b7c0b48df757818
Lee Jong Bub <30468434+bub3690@users.noreply.github.com>
2025-03-18 13:49:49 +0900

    [Gold IV] Title: 동전 1, Time: 452 ms, Memory: 39892 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,23 +1,35 @@
 import sys
+sys.setrecursionlimit(100000)
+
 N,K = tuple(map(int,input().split() ))
 
 coin_list=[]
-dp = [0]*(K+1)
+dp = [[-1]*(K+1) for _ in range(N+1) ] # 코인, 남은돈
 for i in range(N):
     coin_list.append(int(input()))
 
 coin_list.sort()
 
-dp[0]=1
-for coin in coin_list:
-    for i in range(K+1):
-             if i>= coin:
-                dp[i] += dp[i-coin]
+def solve(coin_id,remain):
+    
+    # 기저가 두개.
+    if remain==0:
+        return 1
+    
+    if coin_id ==N or remain < 0:
+        return 0
+    
+    #메모
+    if dp[coin_id][remain] != -1:
+        return dp[coin_id][remain] 
 
-print(dp[K])
+    dp[coin_id][remain] = solve(coin_id,remain-coin_list[coin_id])+solve(coin_id+1,remain)
+    return dp[coin_id][remain]
 
 
+solve(0,K) #coin idx, remain
 
+print(dp[0][K])
 
 
 
