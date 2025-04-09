11c6995c5397794f6b3ae5f495c153e89acbad26
Lee Jong Bub <30468434+bub3690@users.noreply.github.com>
2025-04-09 18:14:01 +0900

    Title: 코드트리 빵, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250409/코드트리 빵/codetree-mon-bread.py
+++ 250409/코드트리 빵/codetree-mon-bread.py
@@ -6,8 +6,8 @@
 
 #요구사항
 
-# 1번순서. 최단거리로 편의점 향하기. 상좌우하.
-# 2번순서. 해당 편의점 도착시, 지날수 없는칸.
+# 1번순서. 최단거리로 편의점 향하기. 상좌우하.(모든사람들이!!!!!)
+# 2번순서. 해당 편의점 도착시, 지날수 없는칸.(다 움직이고 마지막에 체크)
 # 3번순서. t번 사람은 베이스켐프에 들어감. 베이스캠프는 원하는 편의점에 행열 순으로 최단거리로.
 # 베이스캠프가 할당되면 거기를 지날수 없음.
 # 출력 모든 사람이 도착하는 시간.
@@ -16,11 +16,17 @@
 #bfs simulation
 
 #풀이 방법
-# 움직일때 매번 bfs로 경로를 탐색해서 할당한다.
-# moveall -> setbase 순으로 진행
+# 매번 bfs를 통해 한 편의점으로 부터 모든 경로를 측정. 그리고 본인 자리에서 가장 작고 우선순위대로 선택.
+# set base시에 bfs를 통해 자리 정한다.
+# moveall -> 편의점=-1 지정 -> setbase 순으로 진행
 
 #시간
-#5초. O(m*n^2*t). t는 최대 n^2. n^6안에 끝난다. 불안한데.
+#1초. O(m*n^2*t). t는 최대 n^2. n^6안에 끝난다. 불안한데.
+
+# 해당 문제가 틀린이유. base를 정할때도. bfs를 통해서 정해야한다. 최단거리란 매번 바뀐다.
+# 또한 우선순위란건 단순히 bfs를 range 돌린다고 되는게 아니라. 모든 최단거리를 구한후에 그 시작점에서 정하는 것이다.
+
+
 
 from collections import deque
 
@@ -39,6 +45,10 @@ dxs = [-1,0,0,1]
 dys = [0,-1,1,0]
 
 
+visit = [[0]*n for _ in range(n)]
+step  = [[0]*n for _ in range(n)]
+
+
 #cx 낮추기.
 for i in range(m):
     cx_list[i][0] -= 1
@@ -53,35 +63,36 @@ for i in range(n):
 
 
 def search_base(x_cx,y_cx):
-    global base_list
-    # base가 이미 쓰인곳은 못쓴다. 따라서 제거 해줘야함.
-    candidates = []
-    len_base = len(base_list)
-    for i in range(len_base):
-        row =  base_list[i][0]
-        col =  base_list[i][1]
-        diff_row = abs(x_cx-row)
-        diff_col = abs(y_cx-col)
-        distance = diff_row+diff_col
-        candidates.append( (distance,diff_row,row,col) )
-    #tie을 어떻게 처리하지?
-    best_idx, best_val = min(enumerate(candidates),key=lambda x:x[1] )
+    bfs(x_cx,y_cx)
+    # 최단거리인 base 찾기.
+    
+    # i,j 순으로 탐색하면 최솟값이 먼저 담기게 된다. 그래서 조건에 맞음.
+    # 따로 baselist를 관리하지는 않는다.
 
-    return best_idx
+    next_x = -1
+    next_y = -1
+    dist = 1e9
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j]==1 and visit[i][j]==1 and step[i][j]<dist:
+                dist = step[i][j]
+                next_x = i
+                next_y = j
+
+    return next_x,next_y
 
 def set_base(t):
-    if t<m:
-        cx = cx_list[t]
+    time_ind = t -1
+    if t<=m:
+        cx = cx_list[time_ind]
         # 가장 가까운 베이스 찾기.
         x_cx, y_cx = cx[0],cx[1]
-        base_idx=search_base(x_cx,y_cx)
-        base_x, base_y = base_list[base_idx][0],base_list[base_idx][1]
-        del base_list[base_idx]
-
-        # 현재 위치 할당
-        man_list[t]= [ base_x, base_y ]
-        grid[base_x][base_y] = -1
+        base_x,base_y = search_base(x_cx,y_cx)
+        #print("베이스 ",time_ind,base_x,base_y)
         
+        # 현재 위치 할당
+        man_list[time_ind]= [ base_x, base_y ]
+        grid[base_x][base_y] = -1
 
     return
 
@@ -100,72 +111,76 @@ def move(chr_idx):
     # visit 초기화
     x,y = man_list[chr_idx][0], man_list[chr_idx][1]
     cx_x,cx_y = cx_list[chr_idx]
-    next_x,next_y=bfs(x,y,cx_x,cx_y)
-    #print("result :",next_x,next_y)
-    man_list[chr_idx][0]=next_x
-    man_list[chr_idx][1]=next_y
+    bfs(cx_x,cx_y)
+
+    # 4방향을 보면서 갈 곳 정하기.  
+    dist = 1e9
+    next_x = -1
+    next_y = -1
+    for i in range(4):
+        nx = x +dxs[i]
+        ny = y +dys[i]
+        if range_check(nx,ny) and visit[nx][ny]==1 and step[nx][ny]<dist:
+            dist = step[nx][ny]
+            #print("dist ",dist)
+            next_x=nx
+            next_y=ny
+    #print("next : ",next_x,next_y)
+    man_list[chr_idx][0] = next_x
+    man_list[chr_idx][1] = next_y
+
+    # 다른사람도 go를 다맞추면 체크해야한다!!
+    #편의점 탐색 완료시 체크해줘야함!!!!
+
     return
 
-def bfs(x,y, cx_x,cx_y):
-    visited = [[False]*n for _ in range(n)]
-    found = False
 
+# 틀린 부분. cx에서 x,y로 가는길을 탐색하는 방식으로 해야한다. 그리고 step 배열을 바탕으로 불러오기.
+# 아니면 최단거리가 아닌 길이 나온다.
+def bfs(x,y):
     que = deque()
-    que.append((x,y))
-    visited[x][y]= True
-    while(que):
-        now_x,now_y = que.popleft()
-        for i in range(4):
-            next_x = now_x + dxs[i]
-            next_y = now_y + dys[i]
-            if range_check(next_x,next_y) and grid[next_x][next_y]!=-1 and visited[next_x][next_y]==False:
-                # 방문.
-                visited[next_x][next_y] = (now_x,now_y)
-                que.append((next_x,next_y))
-                # 목표,cx 면 탐색을 멈춰야함.
-                if next_x==cx_x and next_y==cx_y:
-                    found= True
-                    #print("found",next_x,next_y)
-                    #print(visited[next_x][next_y])
-                    break
-                
-        if found:
-            break        
+    #초기화
+    for i in range(n):
+        for j in range(n):
+            visit[i][j] = 0
+            step[i][j] = 0
     
-    #x,y 가 나올때 까지 따라 올라가기.
-    next_prev = visited[next_x][next_y]
-    if next_prev == (x,y):
-        # 도착
-        return next_x,next_y
+    que.append((x,y))
+    visit[x][y] = 1
 
-    now_prev = visited[next_x][next_y]
-    while next_prev != (x,y):
-        now_prev = (next_prev[0],next_prev[1]) # 직후 에 가야할곳이 담김.
-        next_prev = visited[next_prev[0]][next_prev[1]]
-    # 바로 이전 경우면?
-
-    return now_prev
+    while que:
+        now_x,now_y=que.popleft()
+        for i in range(4):
+            next_x = now_x+dxs[i]
+            next_y = now_y+dys[i]
+            if range_check(next_x,next_y) and visit[next_x][next_y]==0:
+                step[next_x][next_y] = step[now_x][now_y] + 1
+                visit[next_x][next_y] = 1
+                que.append(( next_x,next_y ))
 
 
 t=0
 while True:
+    t+=1    
     move_all()
+    cnt = 0
+    # 2단계 + 모두 도착했는지 체크.    
+    #print('man ',man_list)
+    #print('cx ',cx_list)
+    for i in range(m):
+        if man_list[i]==cx_list[i]:
+            #print(i,' 도착')
+            grid[man_list[i][0]][man_list[i][1]]= -1
+            cnt+=1
+    
+    if cnt == m:
+        break    
+    
     set_base(t)
-
 
     #print(man_list)
 
-    # 모두 도착했는지 체크.    
-    cnt = 0
-    for i in range(m):
-        if man_list[i]==cx_list[i]:
-            cnt+=1
-    if cnt == m:
-        break
-    
-    t+=1
-
-print(t+1)
+print(t)
 
 
 
